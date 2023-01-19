99de1954af059d3ae1bb800114170b18c243f8dc
Fahim Ahmed <P2595607@my365.dmu.ac.uk>
2023-01-19 22:21:58 +0000

    New changes

--- lab-01/scripts.js
+++ lab-01/scripts.js
@@ -1,3 +1,15 @@
 // alert("Hello!");
 console.log(myElement);
-myElement.textContent = "I've changed this!"
+myElement.textContent = "I've changed this!";
+
+document.getElementById(`theButton`);
+console.log(theButton);
+
+let count = 0;
+
+function change() {
+    count++;
+    theButton.textContent = `Clicked ${count} times!`;
+}
+
+document.getElementById(`theButton`).addEventListener("click", change);
